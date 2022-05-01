8d70475093308101e70f4230d5690f276fedb345
彭梓岸 <1368702766@qq.com>
2022-05-01 10:20:50 +0800

    测试

--- src/axios/index.js
+++ src/axios/index.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 // 引入 nprogress 包美化发送请求时样式
 import Nprogress from "nprogress";
 import 'nprogress/nprogress.css';
+
+// 初始化配置 axios
 const request = axios.create({
     // 配置根路径方便请求
     baseURL: 'http://127.0.0.1:80/',
