1f3ff981e0acb2f5226c4d6cf58d4042e97f0f47
彭梓岸 <1368702766@qq.com>
2022-05-02 16:04:57 +0800

    恢复部分误操作

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-// 管理员模块
-import adminInfo from '@/store/adminInfo'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    adminInfo
   }
 })
