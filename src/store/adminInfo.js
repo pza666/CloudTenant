89c5f0e36ebb8256a30c09b265172229e55a7cbd
彭梓岸 <1368702766@qq.com>
2022-05-02 17:16:19 +0800

    差搜索功能的开发

---
+++ src/store/adminInfo.js
@@ -0,0 +1,14 @@
+import request from '@/axios/index.js'
+const actions = {
+}
+const mutations = {
+}
+const state = {}
+const getters = {}
+export default {
+    namespaced: true,
+    actions,
+    mutations,
+    state,
+    getters
+}
